f4d2385abea228a4053f6a7a118ebd82b9d979df
Abdul Wahab <alwahab20089@gmail.com>
2026-09-11 01:08:48 +0500

    fix render trust proxy

--- server/src/utils/generateToken.js
+++ server/src/utils/generateToken.js
@@ -8,7 +8,7 @@ function setTokenCookie(res, token) {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 }
